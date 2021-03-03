08fa4f791127e79be70ff1e18e409c05c8d8fcb1
Arseny.Polezhaev <arseny.polezhaev@gmail.com>
2021-03-04 01:09:51 +0300

    Support for adding cities to localStorage

--- start.js
+++ start.js
@@ -1,3 +1,10 @@
+const locationTemplate = document.getElementById("location-template");
+const citiesList = document.getElementById("cities");
+document.getElementById("add-city-form").addEventListener("submit", addCityEventHandler);
+for (let i = 0; i < localStorage.length; i++) {
+    addCity(localStorage.getItem(i.toString()), true);
+}
+
 function fetchByCity(city) {
     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=bb00e2c76a482605b246251414383c30`)
         .then(response => response.json());
@@ -8,10 +15,9 @@ function fetchByLocation(location) {
         .then(response => response.json());
 }
 
-function showWeather(weatherInfo, loadingPlaceholderElement, cityNameElement, temperatureElement, weatherElement, extraElementsToShow = []) {
+function showWeather(weatherInfo, loadingPlaceholderElement, cityNameElement, temperatureElement, weatherElement, elementsToShow = []) {
     loadingPlaceholderElement.style.display = "none";
-    weatherElement.style.display = "block";
-    for (const element of extraElementsToShow) {
+    for (const element of elementsToShow) {
         element.style.display = "block";
     }
     cityNameElement.innerHTML = weatherInfo.name;
@@ -24,19 +30,43 @@ function showWeather(weatherInfo, loadingPlaceholderElement, cityNameElement, te
     weatherFeatures[4].children[1].innerHTML = `[${weatherInfo.coord.lat}, ${weatherInfo.coord.lon}]`;
 }
 
-navigator.geolocation.getCurrentPosition(position =>
-    fetchByLocation(position).then(obj => {
-        showWeather(obj, document.getElementById("loading-placeholder-top"),
-            document.getElementById("current-city-name"),
-            document.getElementById("t-current"),
-            document.getElementById("current-weather-features"),
-            [document.getElementById("current-weather-block")]);
-    }), () => {
-    fetchByCity("Moscow,ru").then(obj => {
-        showWeather(obj, document.getElementById("loading-placeholder-top"),
-            document.getElementById("current-city-name"),
-            document.getElementById("t-current"),
-            document.getElementById("current-weather-features"),
-            [document.getElementById("current-weather-block")]);
+function addCity(cityName, isCalledFromStorage) {
+    let newCityElement = locationTemplate.content.cloneNode(true);
+    citiesList.appendChild(newCityElement);
+    newCityElement = citiesList.lastElementChild;
+    fetchByCity(cityName)
+        .then(obj => {
+            showWeather(obj, newCityElement.children[0],
+                newCityElement.querySelector("div.favorite-item-header h3"),
+                newCityElement.querySelector(".t-favourite"),
+                newCityElement.querySelector(".weather-features"),
+                [newCityElement.children[1]]);
+            if (!isCalledFromStorage)
+                localStorage.setItem(localStorage.length.toString(), cityName);
+        }).catch(() => {
+        citiesList.removeChild(newCityElement);
+        alert("Could not retrieve information on this city.");
     });
-});
+}
+
+function addCityEventHandler(event) {
+    event.preventDefault();
+    addCity(document.getElementById("add-city-input").value.trim(), false);
+}
+
+navigator.geolocation.getCurrentPosition(position =>
+    fetchByLocation(position).then(obj =>
+        showWeather(obj, document.getElementById("loading-placeholder-top"),
+            document.getElementById("current-city-name"),
+            document.getElementById("t-current"),
+            document.getElementById("current-weather-features"),
+            [document.getElementById("current-weather-block"), document.getElementById("current-weather-features")]))
+        .catch(() => alert("An error has occurred.")),
+    () =>
+    fetchByCity("Moscow,ru").then(obj =>
+        showWeather(obj, document.getElementById("loading-placeholder-top"),
+            document.getElementById("current-city-name"),
+            document.getElementById("t-current"),
+            document.getElementById("current-weather-features"),
+            [document.getElementById("current-weather-block"), document.getElementById("current-weather-features")]))
+        .catch(() => alert("An error has occurred.")));
